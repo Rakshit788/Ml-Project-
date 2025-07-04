cf8698b53856d6b53d02ec33e1f4ebb463327c92
Rakshit788 <rakshitchopra788@gmail.com>
2025-07-04 23:45:08 +0530

    added the model _trainer file training from colab

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,3 +7,12 @@ from sklearn.model_selection import  train_test_split
 import  pandas as pd 
 
 
+
+@dataclass
+
+class DataIngestionConfig:
+    train_data_path : str =  os.path.join('artifacts','train.csv')
+    test_data_path : str =  os.path.join('artifacts','test.csv')
+    raw_data_csv : str =  os.path.join('artifacts' ,  'data.csv')
+
+
