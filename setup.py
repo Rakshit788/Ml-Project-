757ebaef8063673797d7062bbb54a45309920ebf
Rakshit788 <rakshitchopra788@gmail.com>
2025-07-05 02:48:30 +0530

    worflow completes

--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
 from setuptools import setup, find_packages
-
-
+import os
 
 HYPHEN_E_DOT = '-e .'
 
-def  get_requirmwnts(filepath : str) ->list: 
-    '''
-    This function will return a list of requirements
-    '''
-    requirements =  [] 
-    with open(filepath) as file_obj:
-        requirements = file_obj.readlines()
+def get_requirements(filepath: str) -> list:
+    """
+    Read dependencies from requirements.txt
+    """
+    with open(filepath, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements]
 
-        if HYPHEN_E_DOT in requirements: 
+        if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-    
-    requirements = [req.replace('\n', '') for req in requirements]
 
     return requirements
-    
-
 
 setup(
-    name='my_package',
+    name='mlproject',
     version='0.1.0',
-    author='Rakshit', 
-    author_email='rakshitchopra788@gmail.com' , 
+    author='Rakshit',
+    author_email='rakshitchopra788@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirmwnts('requirement.txt'),
+    install_requires=get_requirements('requirmenst.txt')
 )
-    
